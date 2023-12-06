cfef8a745a8f26534edbb8c61f9a7ea1af94d85f
Aladdin Gamal <engai3030@gmail.com>
2023-12-06 18:45:07 +0200

    add requirments & updata summarization_server file

--- summarization_server.py
+++ summarization_server.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, request
 from summarization_model import get_summary
 
 app = Flask(__name__)
 
-@app.route('/Atlas/summarization', methods=['GET', 'POST'])
-def index():
-    summary = None
-    user_input = ""
-
-    if request.method == 'POST':
-        user_input = request.form['user_input']
-        summary = get_summary(user_input)
-
-    return render_template('summarization.html', user_input=user_input, summary=summary)
-
+@app.route('/atlas/summarization', methods=['POST'])
+def summarization():
+    try:
+        if request.method == 'POST':
+            user_input = request.form['user_input']
+            summary = get_summary(user_input)
+            response_data = {
+                'user_input': user_input,
+                'summary': summary
+            }
+        return jsonify(response_data)
+    except:
+        return "please enter valid text"
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
