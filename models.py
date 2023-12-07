ceec3ebeaafe7bb4a8710694d19f37f993fe0d96
Abdelraouf-Hawash <abdelraouf.hawash@gmail.com>
2023-12-08 00:53:02 +0200

    optimization for old model

--- models.py
+++ models.py
@@ -1,72 +1,60 @@
-"""# Import_library"""
+
 import whisper
 from pytube import YouTube
 from whisper.utils import format_timestamp
 from summa.summarizer import summarize
 from deep_translator import GoogleTranslator
 from langdetect import detect
+import os
+
 model = whisper.load_model("large-v3")
 
-"""# All_Funcation"""
+# Configurations
+UPLOADS_FOLDER = "./uploads"
+if not os.path.exists(UPLOADS_FOLDER): # Create necessary directories
+    os.makedirs(UPLOADS_FOLDER)
+
 
 def download_audio_from_youtube(youtube_url):
+    
     # Create a YouTube object
     yt = YouTube(youtube_url)
-
     # Select the audio stream (you may want to choose the best quality)
     audio_stream = yt.streams.filter(only_audio=True).first()
-
-    # Define the path where you want to save the audio file in your Colab environment
-    save_path = 'output_audio/audio.mp3'  # Change the filename and extension if you prefer a different format
-
-    # Get the actual file name downloaded by pytube
-    actual_file_name = audio_stream.default_filename
-    audio_stream.download(output_path='output_audio', filename='audio.mp3')
-
-    # Rename the downloaded file to your preferred name
-    # os.rename(os.path.join('', actual_file_name), save_path)
-
-    return save_path
+    # download
+    file_name = 'youtube_audio'
+    audio_stream.download(output_path= UPLOADS_FOLDER, filename= file_name)
+    # return with file path
+    return f"./{UPLOADS_FOLDER}/{file_name}"
 
 
 def Transcribe_whisper(audio_path):
-# Transcribe the audio from the video
+
+  # Transcribe the audio from the video
   text_time = model.transcribe(audio_path)
   script=text_time['text']
-  script_time=[]
   script_lang=text_time['language']
+  return script,script_lang
 
-  #with open("transcription_with_timestamps.txt", "w", encoding="utf-8") as output_file:
-  for segment in text_time['segments']:
-      start, end, text = segment["start"], segment["end"], segment["text"]
-      line = f"[{format_timestamp(start)} - {format_timestamp(end)}]  {text}"
-      #output_file.write(line + "\n")
-      script_time.append(line)
-
-  return script,script_lang,script_time
 
 def translate_en_script(input,script_lang):
+
   if(script_lang=='en'):
       return input
-
   else:
     max_chunk_length = 500  # The maximum allowed query length
-    text_chunks = [
-        input[i : i + max_chunk_length] for i in range(0, len(input), max_chunk_length)
-        ]
+    text_chunks = [input[i : i + max_chunk_length] for i in range(0, len(input), max_chunk_length)]
     translated_chunks = [GoogleTranslator(source='auto', target='en').translate(chunk) for chunk in text_chunks]
     translated_text = " ".join(translated_chunks)
     return translated_text
 
 def translate_ar_script(input,script_lang):
+
   if(script_lang=='ar'):
       return input
-
   else:
     max_chunk_length = 500  # The maximum allowed query length
-    text_chunks = [
-        input[i : i + max_chunk_length] for i in range(0, len(input), max_chunk_length)
-        ]
+    text_chunks = [input[i : i + max_chunk_length] for i in range(0, len(input), max_chunk_length)]
     translated_chunks = [GoogleTranslator(source='auto', target='ar').translate(chunk) for chunk in text_chunks]
     translated_text = " ".join(translated_chunks)
     return translated_text
@@ -93,16 +81,19 @@ def translate_ar(input):
         return translated_text
 
 def get_summary(text):
+    
     en_summary = summarize(text,ratio=0.2, language="english")
     ar_summary=translate_ar(en_summary)
 
     return en_summary,ar_summary
 
-"""# Finial_recall_funcation"""
+
+
+### Finial_recall_functions ###
 
 def proccesVideoAudioFile(video_audio_File_Path):
-  print(f" 2video file path {video_audio_File_Path}")
-  script,script_lang,script_time=Transcribe_whisper(video_audio_File_Path)
+
+  script,script_lang=Transcribe_whisper(video_audio_File_Path)
   ar_script=translate_ar_script(script,script_lang)
   en_script=translate_en_script(script,script_lang)
   en_summary,ar_summary=get_summary(en_script)
@@ -111,18 +102,16 @@ def proccesVideoAudioFile(video_audio_File_Path):
       "ar_script":ar_script,
       "en_script":en_script,
       "ar_summary":ar_summary,
-      "en_summary":en_summary,
-      "script_time":script_time,
+      "en_summary":en_summary
   }
 
   return result
 
-# video_link="/content/أبو عبيدة تمكنا من تدمير 20 آلية عسكرية إسرائيلية خلال الـ48 ساعة تدميرا كليا أو جزئيا.mp4"
-# print(proccesVideoAudioFile(video_link))
 
 def proccesYoutubeLinkVideo(link):
+
   audio_path=download_audio_from_youtube(link)
-  script,script_lang,script_time=Transcribe_whisper(audio_path)
+  script,script_lang=Transcribe_whisper(audio_path)
   ar_script=translate_ar_script(script,script_lang)
   en_script=translate_en_script(script,script_lang)
   en_summary,ar_summary=get_summary(en_script)
@@ -131,11 +120,7 @@ def proccesYoutubeLinkVideo(link):
       "ar_script":ar_script,
       "en_script":en_script,
       "ar_summary":ar_summary,
-      "en_summary":en_summary,
-      "script_time":script_time,
+      "en_summary":en_summary
   }
 
   return result
-
-# link="https://youtu.be/WkZfDvnNyAo?si=fyDhHyrfBfriEls7"
-# print(proccesYoutubeLinkVideo(link))
--- server.py
+++ server.py
@@ -1,58 +1,56 @@
 from flask import Flask, render_template, jsonify, request
-from models import proccesVideoAudioFile, proccesYoutubeLinkVideo
+from models import proccesVideoAudioFile, proccesYoutubeLinkVideo, UPLOADS_FOLDER
 import os
 from flask_cors import CORS
 
+
+
+def process_file(file, folder_path= UPLOADS_FOLDER):
+
+    try:
+        file_path = os.path.join(folder_path, file.filename)
+        file.save(file_path)
+        result = proccesVideoAudioFile(file_path)
+        return jsonify({"data": result})
+    except Exception as e:
+        print(f"Error {type(e).__name__} occurred: {e}")
+        return jsonify({"error": "Internal server error. File processing failed."}), 500
+
+
 app = Flask(__name__)
 CORS(app)
-
-# Configuration
-UPLOAD_VIDEOS_FOLDER = "./uploaded_videos"
-UPLOAD_AUDIOS_FOLDER = "./uploaded_audios"
-PORT = 8000
-
-# Create necessary directories
-def create_directory(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-create_directory(UPLOAD_VIDEOS_FOLDER)
-create_directory(UPLOAD_AUDIOS_FOLDER)
+PORT= 8000
 
 @app.route("/", methods=["GET"])
 def index():
     """Render the index page."""
     return render_template("index.html")
 
-def process_file(file, folder_path, process_function):
-    """Generic file processing function."""
-    try:
-        file_path = os.path.join(folder_path, file.filename)
-        file.save(file_path)
-        result = process_function(file_path)
-        return jsonify({"data": result})
-    except Exception as e:
-        print(f"Error {type(e).__name__} occurred: {e}")
-        return jsonify({"error": "Internal server error. File processing failed."}), 500
+# @app.route("/processAudio", methods=["POST"])
+# def process_audio():
+#     try:
+#         audio = request.files.get("audio")
+#         if audio and audio.filename:
+#             file_path = os.path.join(UPLOADS_FOLDER, "audio")
+#             audio.save(file_path)
+#             result = proccesVideoAudioFile(file_path)
+#             return jsonify({"data": result})
+#         else:
+#             return jsonify({"error": "Invalid audio file. Please upload a valid file."}), 400
+        
+#     except Exception as e:
+#         print(f"Error {type(e).__name__} occurred: {e}")
+#         return jsonify({"error": "Internal server error. File processing failed."}), 500
 
-@app.route("/getAudioFile", methods=["POST"])
-def process_audio():
-    """Process uploaded audio files."""
-    audio = request.files.get("audio")
-    print(type(audio))
-    if audio and audio.filename:
-        return process_file(audio, UPLOAD_AUDIOS_FOLDER, proccesVideoAudioFile)
-    return jsonify({"error": "Invalid audio file. Please upload a valid file."}), 400
+# @app.route("/processVideo", methods=["POST"])
+# def process_video():
+#     """Process uploaded video files."""
+#     video = request.files.get("video")
+#     if video and video.filename:
+#         return process_file(video, UPLOAD_VIDEOS_FOLDER, proccesVideoAudioFile)
+#     return jsonify({"error": "Invalid video file. Please upload a valid file."}), 400
 
-@app.route("/getVideoFile", methods=["POST"])
-def process_video():
-    """Process uploaded video files."""
-    video = request.files.get("video")
-    if video and video.filename:
-        return process_file(video, UPLOAD_VIDEOS_FOLDER, proccesVideoAudioFile)
-    return jsonify({"error": "Invalid video file. Please upload a valid file."}), 400
-
-@app.route("/getYoutubeVideoLink", methods=["POST"])
+@app.route("/processYoutubeLink", methods=["POST"])
 def process_youtube_link():
     """Process YouTube video links."""
 
