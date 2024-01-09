f780700af6b2925edef0a775b5a51fa53dfec208
Shady Mohamed <96765781+ShadiM0hamed@users.noreply.github.com>
2024-01-09 17:01:47 +0200

    Update translate_model.py to make split with words not characters

--- translate_model.py
+++ translate_model.py
@@ -1,15 +1,29 @@
 from deep_translator import GoogleTranslator
-from langdetect import detect
+import re
 
+def word_tokenization(text):
+    words = re.findall(r'\b\w+\b', text)
+    return words
 
-
-def translate(input,script_lang):
+def translate(input, script_lang):
     max_chunk_length = 500  # The maximum allowed query length
-    text_chunks = [
-        input[i : i + max_chunk_length] for i in range(0, len(input), max_chunk_length)
-        ]
-    translated_chunks = [GoogleTranslator(source='auto', target=script_lang).translate(chunk) for chunk in text_chunks]
+    words = word_tokenization(input)
+    
+    translated_chunks = []
+    current_chunk = ""
+
+    for word in words:
+        if len(current_chunk) + len(word) <= max_chunk_length:
+            current_chunk += word + " "
+        else:
+            translated_chunk = GoogleTranslator(source='auto', target=script_lang).translate(current_chunk.strip())
+            translated_chunks.append(translated_chunk)
+            current_chunk = word + " "
+
+    # Translate the last chunk
+    if current_chunk:
+        translated_chunk = GoogleTranslator(source='auto', target=script_lang).translate(current_chunk.strip())
+        translated_chunks.append(translated_chunk)
+
     translated_text = " ".join(translated_chunks)
     return translated_text
-
-
